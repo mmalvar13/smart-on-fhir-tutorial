7814a1047d11f49bf667bd8461193795ed58e8a8
Monica Alvarez <monica@rs21.io>
2020-02-18 22:52:46 -0700

    40

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -65,10 +65,11 @@
     window.addNewPatient = function(p) {
       console.log('inside add new patient')
 
-      var first = document.getElementById("fname").value
-      var last = document.getElementById("lname").value
-      console.log(first)
-      console.log(last)
+      var first = document.getElementById("firstname");
+
+      var last = document.getElementById("lastname").value;
+      console.log(first);
+      console.log(last);
 
 
       console.log(p)
@@ -130,7 +131,7 @@
     }
 
     function onReady(smart) {
-      console.log('39')
+      console.log('40')
       if (smart.hasOwnProperty('patient')) {
         console.log('inside smart has own property')
         var patient = smart.patient;
