66d0ebea775ac9a17ea3716f3526b8d28882ddf5
Monica Alvarez <monica@rs21.io>
2020-02-18 18:14:03 -0700

    13

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,7 +8,7 @@
     }
 
     function onReady(smart) {
-      console.log('12')
+      console.log('13')
       if (smart.hasOwnProperty('patient')) {
         console.log('inside smart has own property')
         var patient = smart.patient;
